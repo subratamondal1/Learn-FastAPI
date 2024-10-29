69dd16b155d0158aa30da6ec0d9cef97a662d229
subratamondal1 <subratasubha2@gmail.com>
2024-10-29 10:49:17 +0530

    Complete 1. First steps with FastAPI

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Learn FastApi
+<h1 align="center">Learn FastApi</h1>
+
+<p align="center">
+  <img src="https://skillicons.dev/icons?i=python,fastapi,docker,azure,git,github&perline=7" />
+</p>
--- bookstore/main.py
+++ bookstore/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 
 from .routers import authors, books
 
@@ -7,6 +8,19 @@ app.include_router(router=books.router)
 app.include_router(router=authors.router)
 
 
+@app.exception_handler(exc_class_or_status_code=HTTPException)
+async def custom_exception_handler(request, exc) -> JSONResponse:
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.detail, "message": "Oops! Something went wrong."},
+    )
+
+
 @app.get(path="/")
 async def read_root() -> dict[str, str]:
     return {"data": "Welcome to Bookstore!"}
+
+
+@app.get(path="/error_endpoint")
+async def raise_exception():
+    raise HTTPException(status_code=400)
